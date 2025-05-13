2b92b6672d1589af8fbd941fa95cd8049ae6b273
bernat alcudia <bernatalcudia@gmail.com>
2025-05-13 10:32:28 +0200

    Hashed password in register

--- server/controllers/members.controller.js
+++ server/controllers/members.controller.js
@@ -3,8 +3,15 @@ const bcryptjs = require("bcryptjs")
 
 
 const login = async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
+    const memberUsername = req.body.username
+    const memberPassword = req.body.password
+
+    if (!memberPassword || !memberName || !memberUsername) {
+        res.status(400).send("Missing required info")
+        return
+    }
+
+    const hashedPassword = bcryptjs.hashSync(memberPassword)
 
     const user = await Member.findOne({ where: { user: username } })
     if (!user) {
@@ -16,16 +23,38 @@ const login = async (req, res) => {
         res.status(400).send("Incorrect password")
     }
     res.status(201).send({ key: user.id })
+
+
+
+
+    try {
+
+        const existingUser = await Member.findOne({ where: { user: memberUsername } })
+        if (!existingUser) {
+            res.status(400).send("User already exists")
+            return
+        }
+
+        const createMember = async (req, res) => {
+            const memberName = req.body.name
+            const createdMember = await Member.create({
+                name: memberName,
+                user: memberUsername,
+                registrationDate: new Date(),
+                password: hashedPassword,
+            })
+            res.status(201).send({ id: createMember.id })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Unexpected register error")
+    }
+
+
 }
 
-const createMember = async (req, res) => {
-    const memberName = req.body.name
-    const createdMember = await Member.create({
-        name: memberName,
-        registrationDate: new Date(),
-    })
-    res.status(201).send({ id: createMember.id })
-}
+
 
 exports.createMember = createMember;
 exports.login = login
